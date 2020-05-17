7db6c4f716b3ec7c93f0a52d10a248f61b1124b0
Francisco Pedraza <pacopedraza87@gmail.com>
2020-05-17 16:28:24 -0700

    Make it work with float values
    Apparently drinking some nice ales make this work.
    Salud!
    
    Signed-off-by: Francisco Pedraza <pacopedraza87@gmail.com>

--- downsample_audio_signal.py
+++ downsample_audio_signal.py
@@ -8,10 +8,14 @@ def downsample_by_2(input_value1, input_value2, val=''):
     print("Calculating downsample by 2")
     print(f"Original values in list: {input_value1}\n{input_value2}")
     if val:
+        print(f"val={val}")
         output_values = np.convolve(input_value1, input_value2, val)
-    output_values = np.convolve(input_value1, input_value2)
-    print(f"output down sample: {output_values}")
-    return output_values
+        print(f"output when val {output_values}")
+        return output_values
+    else:
+        output = np.convolve(input_value1, input_value2)
+        print(f"output down sample: {output_values}")
+        return output_values
 
 def decimate_by_2(output_values):
     print("Calculating decimate by 2")
