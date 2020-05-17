c99bbba5f64e21c93b894c3cf58b67a65f0345f0
Francisco Pedraza <pacopedraza87@gmail.com>
2020-05-17 11:44:17 -0700

    Adds Downsample for Audio signal
    Added the two main functions
    1.  decimate_by_2
    2.  downsample_by_2
    implemented with numpy and scipy
    
    Signed-off-by: Francisco Pedraza <pacopedraza87@gmail.com>

---
+++ downsample_audio_signal.py
@@ -0,0 +1,18 @@
+import numpy as np
+import scipy as sc
+from scipy import signal
+
+
+def downsample_by_2(input_value1, input_value2):
+    # Apply low pass filtering by 2 convolve
+    print("Calculating downsample by 2")
+    print(f"Original values in list: {input_value1}\n{input_value2}")
+    output_values = np.convolve(input_value1, input_value2)
+    print(f"output downsample: {output_values}")
+    return output_values
+
+def decimate_by_2(output_values):
+    print("Calculating decimate by 2")
+    decimation = sc.signal.decimate(output_values, 2)
+    print(f"Decimation is: {decimation}")
+    return decimation
