96a8d75f1e9d6658a6ee480bcae100f0a129b14a
Francisco Pedraza <pacopedraza87@gmail.com>
2020-05-17 21:45:34 -0700

    Adds more test cases for low pass filtering
    Signed-off-by: Francisco Pedraza <pacopedraza87@gmail.com>

--- test_downsample_audio_signal.py
+++ test_downsample_audio_signal.py
@@ -9,22 +9,21 @@ class DecimateTestCase(unittest.TestCase):
     2. Decimate by two
     """
 
-    #def test_decimate(self):
-    #    """
-    #    Tests lips the second array before `sliding` 
-    #    the two across one another.
-    #    """
-    #    output1 = downsample_by_2(input_convolve, input_convolve2)
-    #    self.assertEqual(expected_convolve1, output1)
-    #    self.assertEqual(output1, expected_convolve1)
+    def test_decimate(self):
+        """
+        Tests lips the second array before `sliding`
+        the two across one another.
+        """
+        output_decimate= downsample_by_2(input_convolve, input_convolve2)
+        self.assertEqual(output_decimate.tolist(), expected_convolve1)
 
-    #def test_decimate_middle(self):
-    #    """
-    #    Tests Contains boundary effects, where zeros 
-    #    are taken into account.
-    #    """
-    #    output2 = downsample_by_2(input_conv_middle, input_conv_middle2, middle_val)
-    #    self.assertEqual(expected_conv_middle, output2)
+    def test_decimate_middle(self):
+        """
+        Tests Contains boundary effecs,
+        where zeros are taken into account.
+        """
+        output_middle = downsample_by_2(input_conv_middle, input_conv_middle2, middle_val)
+        self.assertEqual(output_middle.tolist(), expected_conv_middle)
 
     def test_decimate_same(self):
         """
@@ -32,20 +31,20 @@ class DecimateTestCase(unittest.TestCase):
         where they completely overlap.
         """
         output_same = downsample_by_2(input_conv_middle, input_conv_middle2, same_val)
-        #self.assertEqual(expected_same, output_same)
         self.assertEqual(output_same, expected_same)
 
 # Test variables
 input_convolve = [1, 2, 3]
 input_convolve2 = [0, 1, 0.5]
-expected_convolve1 = array(str('u'),'[0. , 1. , 2.5, 4. , 1.5]')
+expected_convolve1 = [0.,  1.,  2.5, 4.,  1.5]
+
 
 input_conv_middle = [1,2,3]
 input_conv_middle2 = [0,1,0.5]
 middle_val = 'same'
-expected_conv_middle = array(str('u'), '[0., 1. , 2.5, 4. ]')
+expected_conv_middle  = [1.,  2.5, 4. ]
 
-#expected_same = array(str('u'), [2.5])
+
 expected_same = array('f', [2.5])
 same_val = 'valid'
 
